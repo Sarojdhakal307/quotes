143e141396c9504827bd28bb7fbfc599aa36da15
SAROJ DHAKAL <134726799+Sarojdhakal307@users.noreply.github.com>
2024-03-17 22:23:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const quote1 = document.getElementById("Quote");
+const writer = document.getElementById("Writer");
+const button = document.getElementById("getQuote");
+
+
+async function randomQuote() {
+    const response = await fetch('https://api.quotable.io/random')
+    const quote = await response.json()
+    
+    // Output the quote and author name
+    console.log(quote.content);
+    quote1.innerText = quote.content;
+    console.log(`- ${quote.author}`);
+    writer.innerText = quote.author;
+  }
+button.addEventListener('click' , function() {
+
+
+    console.log('I am clicked');
+    randomQuote();
+    quote1.style.display = 'block';
+    writer.style.display = 'block';
+});
+
+
